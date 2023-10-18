3534b2d7a76ced554ba5d173c959b62303ab12bb
Tess Thomas <104388884+tessthom@users.noreply.github.com>
2023-10-18 09:22:59 -0400

    init form

--- index.php
+++ index.php
@@ -8,13 +8,43 @@ ini_set('display_errors', 1);
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Batched Cocktail Calculator</title>
+  <title>Batched Old Fashioned Calculator</title>
   <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-  
+  <main>
+    <h1>Batched Old Fashioned Calculator</h1>
+    <?php if (!empty($error_message)) { ?>
+      <p class="error"><?php echo $error_message; ?></p>
+    <?php } ?>
+
+    <form action="display_results.php" method="post">
+      <div id="data">
+        <label>Number of servings:</label>
+        <input type="text" name="servings" value="<?php echo $servings; ?>"><br>
+
+        <label>Sweetness level:</label>
+        <select name="sweetness">
+          <option value="Just enough"></option> <!-- 0.25oz -->
+          <option value="Classic specs"></option> <!-- 0.5oz -->
+          <option value="Sweeter"></option> <!-- 0.75oz -->
+          <option value="Wisconsin sweet"></option> <!-- 1.0oz -->
+        </select>
+
+        <p>*Calculation for a 1:1 simple syrup</p>
+      </div>
+
+      <div class="buttons">
+        <label>&nbsp;</label>
+        <input type="submit" value="Calculate">
+      </div>
+    </form>
+  </main>
 </body>
+
 </html>
