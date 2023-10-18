7ba33ac6444ffccbeeb8fb02b563824124936639
Tess Thomas <104388884+tessthom@users.noreply.github.com>
2023-10-18 09:01:50 -0400

    html init

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'batch.php';
+
+// display any errors
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Batched Cocktail Calculator</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  
+</body>
+</html>
